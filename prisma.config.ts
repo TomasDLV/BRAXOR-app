faa183a4d16d4276eb7c72e0e05419352f683e1d
TomasDLV <tomas01dlv@gmail.com>
2026-03-17 23:25:09 -0300

    .3

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,4 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
-  migrate: {
-    seed: {
-      run: "npx tsx prisma/seed.ts",
-    },
-  },
 });
